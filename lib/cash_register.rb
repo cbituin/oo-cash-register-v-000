eff8036c061cd0794d1ef02872b4ae58f2d80e7f
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-11-27 13:31:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
 
+class CashRegister
+  
+  attr_accessor :total
+
+def initialize
+  @total = 0
+end
+
+def self.new
+end
+
+def total
+end
+
+def add_item(title, price)
+end
+
+def apply_discount
+end
+
+def items
+end
+
+def void_last_transaction
+end
+
+end
